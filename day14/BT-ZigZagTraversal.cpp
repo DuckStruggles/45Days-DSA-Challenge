5206a570a83179bc032e16bfafa385c4e51937b7
Kuhu <89199844+DuckStruggles@users.noreply.github.com>
2026-05-12 23:19:09 +0530

    Day 14 done

---
+++ day14/BT-ZigZagTraversal.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+class Node {
+    public:
+      int data;
+      Node* left;
+      Node* right;
+
+      Node(int value) {
+        data = value;
+        left=NULL;
+        right=NULL;
+      }
+};
+
+vector<vector<int>> levelOrder(Node* root) {
+    queue<Node*> q;
+    vector<vector<int>> ans;
+
+    int flag=1;
+
+    if(root==NULL) return ans;
+
+    q.push(root);
+
+    while(!q.empty()) {
+        int s=q.size(); //no of nodes on this level
+        vector<int> lvl;
+
+        for(int i=0; i<s; i++) {
+            Node* temp = q.front();
+            q.pop();
+
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+
+            lvl.push_back(temp->data);
+        }
+        if(flag%2==0) reverse(lvl.begin(), lvl.end());
+        flag++;
+        ans.push_back(lvl);
+    }
+    return ans;
+}
+
+int main() {
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+
+    vector<vector<int>> ans = levelOrder(root);
+
+    for(int i=0; i<ans.size(); i++) {
+        for(int j=0; j<ans[i].size(); j++) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
